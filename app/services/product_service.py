272f3c727389fe4af3d762b4ddb7585826ca666e
ph-gusmao <fox_max_power@outlook.com>
2026-06-03 18:06:21 -0300

    feat: add product service

---
+++ app/services/product_service.py
@@ -0,0 +1,31 @@
+from app.models.product_model import Product
+from app.extensions import db
+
+
+def create_product(name, price, stock):
+    product = Product(name=name, price=price, stock=stock)
+    db.session.add(product)
+    db.session.commit()
+    return product
+
+
+def get_all_products():
+    return Product.query.all()
+
+
+def get_product_by_id(product_id):
+    return Product.query.get(product_id)
+
+
+def update_product(product, data):
+    product.name = data.get("name", product.name)
+    product.price = data.get("price", product.price)
+    product.stock = data.get("stock", product.stock)
+
+    db.session.commit()
+    return product
+
+
+def delete_product(product):
+    db.session.delete(product)
+    db.session.commit()
