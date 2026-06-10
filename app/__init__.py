e1c43f1d96e1af07d11525c727705bb8e3fc6374
ph-gusmao <fox_max_power@outlook.com>
2026-06-10 01:39:42 -0300

    feat: add refresh token in init py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.extensions import db, jwt
 from app.routes.auth_routes import auth_bp
 from app.routes.product_routes import product_bp
+from datetime import timedelta
 
 
 def create_app():
@@ -10,6 +11,8 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = "super_secret_key"
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(product_bp)
 
