a7bc32f3d0c525de393ad0f9b5de5c3da42bd557
ph-gusmao <fox_max_power@outlook.com>
2026-06-17 01:40:53 -0300

    feat: add profile test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -28,3 +28,24 @@ def test_login(client):
 
     assert "access_token" in data
     assert "refresh_token" in data
+
+
+def test_profile(client):
+
+    username = f"teste_{uuid.uuid4()}"
+
+    client.post("/auth/register", json={"username": username, "password": "12345"})
+
+    login_response = client.post(
+        "/auth/login", json={"username": username, "password": "12345"}
+    )
+
+    token = login_response.get_json()["access_token"]
+
+    response = client.get("/auth/profile", headers={"Authorization": f"Bearer {token}"})
+
+    data = response.get_json()
+
+    assert response.status_code == 200
+
+    assert data["username"] == username
